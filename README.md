8ad9167ee031cd5c2ca233a03694e980a3fd7fe0
Aaron Tobey <abtobey@gmail.com>
2020-12-21 14:29:06 -0600

    added readme and minor CSS improvements

---
+++ README.md
@@ -0,0 +1,42 @@
+# Poke Lookup
+
+## This application uses the poke api to look up basic stats on a pokemon entered into the search box. An error message is generated if the user types in a name that is not a pokemon or leaves the field blank.
+
+Table of contents:
+  * [Installation Instructions](#installation-instructions)
+  * [Usage Instructions](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Contact info](#contact-info)
+---
+
+## Installation instructions: 
+
+In git bash, enter git clone https://github.com/abtobey/pokeLookup.git to clone the repository. In the terminal, enter npm install jest to install the testing package.
+---
+
+## Usage: 
+
+
+---
+
+## Contributing: 
+
+
+---
+
+## Tests:
+
+
+---
+
+##License:
+
+This project is covered under the MIT license.
+
+---
+
+## Questions: 
+email: abtobey@gmail.com
+github: abtobey
--- script.js
+++ script.js
@@ -8,6 +8,7 @@ document.getElementById("submitButton").addEventListener("click", function(){
     let choice=pokemonSelect.value.toLowerCase();
     if(choice.length==0){
         warning.innerText="Please enter a pokemon"
+        return("No Pokemon Entered")
     }else{
         warning.innerText=""
         fetchPokemon(choice)
@@ -15,23 +16,15 @@ document.getElementById("submitButton").addEventListener("click", function(){
 
 })
 
-function badReq(name){
-    warning.innerText=("no pokemon with name " + name)
-
+function capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1)
 }
 
-function fetchPokemon(pokemonName){
-const queryURL=baseUrl + pokemonName
-const capitalized=pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response){
-    console.log(response)
+function addDiv(response){
     $(`
     <div class="card pokeCard col-8">
     <img src=${response.sprites.front_default} class=sprite><br>
-    <h2>Name: ${capitalized}</h2>
+    <h2>Name: ${capitalize(response.name)}</h2>
         <h3>ID: ${response.id}</h3>
         <h4>HP: ${response.stats[0].base_stat}</h4>
         <h4>Attack: ${response.stats[1].base_stat}</h4>
@@ -43,6 +36,23 @@ $.ajax({
     `).prependTo("#results");
     //clear input box
     pokemonSelect.value=""
+}
+
+function badReq(name){
+    warning.innerText=name +" not found"
+
+    return(("No pokemon found"))
+}
+
+function fetchPokemon(pokemonName){
+const queryURL=baseUrl + pokemonName
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response)
+    addDiv(response)
+    return response.id
 //catch will happen if we send a request with a name of a pokemon that doesn't exist
 }).catch(err => badReq(pokemonName))
 }
